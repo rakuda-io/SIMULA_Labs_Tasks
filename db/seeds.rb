322b50f0dc14b81e509fa220a199efa08dc7883e
Yuki Kubota <rakudalms@gmail.com>
2021-09-27 06:14:10 +0900

    RSpecリファクタリング、shered_exsampleはなしに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#担任
 Teacher.create!(
   [
     {
@@ -11,12 +12,13 @@ Teacher.create!(
   ]
 )
 
+#科目
 Subject.create!(
   [
     {
       subject_id: 1,
       teacher_id: 1,
-      title: 'グレートなティーチャーになるための教育論講座',
+      title: 'グレートなティーチャーによる教育論',
       weekday: 'monday',
       period: 5,
     },
@@ -30,13 +32,21 @@ Subject.create!(
     {
       subject_id: 3,
       teacher_id: 1,
-      title: '喧嘩の仕方',
-      weekday: 'sunday',
+      title: '必勝！実践空手講座',
+      weekday: 'wednesday',
       period: 1,
+    },
+    {
+      subject_id: 4,
+      teacher_id: 2,
+      title: '人生を勝ち抜いていく心構え',
+      weekday: 'friday',
+      period: 5,
     },
   ]
 )
 
+#授業
 Lecture.create!(
   [
     {
@@ -60,20 +70,32 @@ Lecture.create!(
     {
       lecture_id: 4,
       subject_id: 2,
-      title: '鬼の手の使い方実演',
+      title: '鬼の手の使い方実演会',
       date: '2021-04-23',
     },
     {
       lecture_id: 5,
       subject_id: 3,
-      title: '鬼sub1',
-      date: '2021-05-29',
+      title: '必勝の型',
+      date: '2021-05-19',
     },
     {
       lecture_id: 6,
       subject_id: 3,
-      title: '鬼sub2',
-      date: '2021-04-22',
+      title: '空手の心構えとは',
+      date: '2021-04-21',
+    },
+    {
+      lecture_id: 7,
+      subject_id: 4,
+      title: '将来ビジョンのイメージ',
+      date: '2021-09-17',
+    },
+    {
+      lecture_id: 8,
+      subject_id: 4,
+      title: '成功者に学ぶ人生論',
+      date: '2021-09-10',
     },
   ]
 )
--- spec/requests/api/v1/subjects_spec.rb
+++ spec/requests/api/v1/subjects_spec.rb
@@ -6,20 +6,6 @@ RSpec.describe '#search' do
     #ベースとなるURL
     let!(:base_url) {'http://localhost:3000/api/v1/subjects?keyword='}
 
-    #リクエストに対してSubjectかTeacherどちらか1データ分のレスポンスデータ
-    let(:one_hit_res) {JSON.parse(Net::HTTP.get(URI.parse(one_hit_data_url)))}
-    #リクエストに対してSubjectかTeacherどちらか2データ分のレスポンスデータ
-    let(:two_hits_res) {JSON.parse(Net::HTTP.get(URI.parse(two_hits_data_url)))}
-    #リクエストに対してSubjectかTeacher両方の全レスポンスデータ
-    let(:both_hits_res) {JSON.parse(Net::HTTP.get(URI.parse(both_hits_data_url)))}
-
-    shared_examples '並び順' do
-      it 'Lectureが開講年月日(date)において昇順にソートされていること' do
-        lectures = one_hit_res['subjects'][0]['lectures'].map {|l| l['date']}
-        expect(lectures).to eq(["2021-04-12", "2021-04-19"])
-      end
-    end
-
     #searchアクションのケース1のテスト
     context '検索キーワードになにも該当しないとき' do
       let(:no_hit_url) {base_url + URI.encode_www_form_component('殺せんせー')}
@@ -36,12 +22,14 @@ RSpec.describe '#search' do
 
     #searchアクションのケース2のテスト
     context '検索キーワードがSubject(科目)のみで該当するとき' do
-      let(:one_hit_data_url) {base_url + URI.encode_www_form_component('グレート')}
-      let(:two_hits_data_url) {base_url + URI.encode_www_form_component('講座')}
+      let!(:one_hit_data_url) {base_url + URI.encode_www_form_component('グレート')}
+      let!(:two_hits_data_url) {base_url + URI.encode_www_form_component('講座')}
+      let(:one_hit_res) {JSON.parse(Net::HTTP.get(URI.parse(one_hit_data_url)))}
+      let(:two_hits_res) {JSON.parse(Net::HTTP.get(URI.parse(two_hits_data_url)))}
 
       context '該当するデータが1個の場合' do
         it '1個のデータを返すこと' do
-          expect(one_hit_res['subjects'][0]['title']).to eq("グレートなティーチャーになるための教育論講座")
+          expect(one_hit_res['subjects'][0]['title']).to eq("グレートなティーチャーによる教育論")
         end
 
         it 'ステータスコードが200であること' do
@@ -49,13 +37,16 @@ RSpec.describe '#search' do
           expect(response).to have_http_status(200)
         end
 
-        it_behaves_like '並び順'
+        it 'Lectureが開講年月日(date)において昇順にソートされていること' do
+          lectures = one_hit_res['subjects'][0]['lectures'].map {|l| l['date']}
+          expect(lectures).to eq(["2021-04-12", "2021-04-19"])
+        end
       end
 
       context '該当するデータが2個の場合' do
         it '2個のデータを返すこと' do
-          expect(two_hits_res['subjects'][0]['title']).to eq("グレートなティーチャーになるための教育論講座")
-          expect(two_hits_res['subjects'][1]['title']).to eq("悪霊・鬼の倒し方実践講座")
+          expect(two_hits_res['subjects'][0]['title']).to eq("悪霊・鬼の倒し方実践講座")
+          expect(two_hits_res['subjects'][1]['title']).to eq("必勝！実践空手講座")
         end
 
         it 'ステータスコードが200であること' do
@@ -63,7 +54,10 @@ RSpec.describe '#search' do
           expect(response).to have_http_status(200)
         end
 
-        it_behaves_like '並び順'
+        it 'Lectureが開講年月日(date)において昇順にソートされていること' do
+          lectures = two_hits_res['subjects'][0]['lectures'].map {|l| l['date']}
+          expect(lectures).to eq(["2021-04-16", "2021-04-23"])
+        end
       end
     end
 
@@ -71,6 +65,8 @@ RSpec.describe '#search' do
     context '検索キーワードがTeacher(教師)のみで該当するとき' do
       let(:one_hit_data_url) {base_url + URI.encode_www_form_component('鬼塚')}
       let(:two_hits_data_url) {base_url + URI.encode_www_form_component('先生')}
+      let(:one_hit_res) {JSON.parse(Net::HTTP.get(URI.parse(one_hit_data_url)))}
+      let(:two_hits_res) {JSON.parse(Net::HTTP.get(URI.parse(two_hits_data_url)))}
 
       context '該当するデータが1個の場合' do
         it '1個のデータを返すこと' do
@@ -82,13 +78,23 @@ RSpec.describe '#search' do
           expect(response).to have_http_status(200)
         end
 
-        it_behaves_like '並び順'
+        it 'Lectureが開講年月日(date)において昇順にソートされていること' do
+          lectures = one_hit_res['subjects'][0]['lectures'].map {|l| l['date']}
+          expect(lectures).to eq(["2021-04-12", "2021-04-19"])
+        end
       end
 
       context '該当するデータが2個の場合' do
+        #要項　　seeds.rbによって教師が2人いる状況であること
+        it 'teacher(教師)が2人存在すること' do
+          teacher_name_array = two_hits_res['subjects'].map do |data|
+            data['teacher']['name']
+          end
+          expect(teacher_name_array.uniq.length).to eq(2)
+        end
         it '2個のデータを返すこと' do
           expect(two_hits_res['subjects'][0]['teacher']['name']).to eq("鬼塚先生")
-          expect(two_hits_res['subjects'][2]['teacher']['name']).to eq("鵺野先生")  #######あとで2->1直す
+          expect(two_hits_res['subjects'][2]['teacher']['name']).to eq("鵺野先生")
         end
 
         it 'ステータスコードが200であること' do
@@ -96,34 +102,33 @@ RSpec.describe '#search' do
           expect(response).to have_http_status(200)
         end
 
-        it_behaves_like '並び順'
+        it 'Lectureが開講年月日(date)において昇順にソートされていること' do
+          lectures = two_hits_res['subjects'][0]['lectures'].map {|l| l['date']}
+          expect(lectures).to eq(["2021-04-12", "2021-04-19"])
+        end
       end
     end
 
     #searchアクションのケース4のテスト
     context '検索キーワードが両方に該当するとき' do
       let(:both_hits_data_url) {base_url + URI.encode_www_form_component('鬼')}
+      let(:both_hits_res) {JSON.parse(Net::HTTP.get(URI.parse(both_hits_data_url)))}
 
       it '該当するデータすべてを返すこと' do
         expect(both_hits_res['subjects'][0]['title']).to eq("悪霊・鬼の倒し方実践講座")
         expect(both_hits_res['subjects'][1]['teacher']['name']).to eq("鬼塚先生")
-        expect(both_hits_res['subjects'][2]['teacher']['name']).to eq("鬼塚先生")  #######あとで消す
+        expect(both_hits_res['subjects'][2]['teacher']['name']).to eq("鬼塚先生")
       end
 
       it 'ステータスコードが200であること' do
         get both_hits_data_url
         expect(response).to have_http_status(200)
       end
-      
+
       it 'Lectureが開講年月日(date)において昇順にソートされていること' do
         lectures = both_hits_res['subjects'][1]['lectures'].map {|l| l['date']}
         expect(lectures).to eq(["2021-04-12", "2021-04-19"])
       end
-    end
-
-
-    xit 'subject(科目)が2つであること' do
-      expect(Lecture.all).to eq(20)
     end
   end
 
